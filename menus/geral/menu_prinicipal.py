0ec6d531ddbd945a86d6d0c0be31b5055a4be3aa
João Pedro <joaooppeedroo325@gmail.com>
2026-06-04 17:51:02 -0300

    Só umas modificações

---
+++ menus/geral/menu_prinicipal.py
@@ -0,0 +1,22 @@
+def exibir_menu_principal():
+
+    opcao = input("""
+
+=========================================
+     SISTEMA DE GERENCIAMENTO AÉREO
+=========================================
+
+Bem-vindo(a)!
+
+1 - Área do Cliente
+2 - Área do Piloto
+3 - Torre de Controle
+4 - Gerência
+
+0 - Encerrar Sistema
+
+=========================================
+Escolha uma opção:
+""")
+
+    return opcao
--- models/aeronave.py
+++ models/aeronave.py
@@ -1,6 +1,6 @@
 class aeronave:
-       def __init__(self, modelo, prefixo, capacidade_de_passageiros, situação):
+       def __init__(self, modelo, prefixo, capacidade_de_passageiros, status):
            self.modelo = modelo
            self.prefixo = prefixo
            self.capacidade_de_passageiros = capacidade_de_passageiros
-           self.situação = situação
+           self.situação = status
