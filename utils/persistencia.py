5972d852f6eff4da66ea7d77348903313310853b
João Pedro <joaooppeedroo325@gmail.com>
2026-06-28 15:24:41 -0300

    Correções minimas

--- utils/persistencia.py
+++ utils/persistencia.py
@@ -5,7 +5,7 @@ def carregar_clientes():
 
     try:
 
-        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto CLI/data/pilotos.json", "r", encoding="utf-8") as arquivo:
+        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto-CLI/data/clientes.json", "r", encoding="utf-8") as arquivo:
 
             return json.load(arquivo)
 
@@ -16,7 +16,7 @@ def carregar_clientes():
 
 def salvar_clientes(clientes):
 
-        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto CLI/data/pilotos.json", "w", encoding="utf-8") as arquivo:
+        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto-CLI/data/clientes.json", "w", encoding="utf-8") as arquivo:
 
             json.dump(clientes, arquivo, indent=4, ensure_ascii=False)
 
@@ -26,7 +26,7 @@ def carregar_pilotos():
 
     try:
 
-        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto CLI/data/pilotos.json", "r", encoding="utf-8") as arquivo:
+        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto-CLI/data/pilotos.json", "r", encoding="utf-8") as arquivo:
 
             return json.load(arquivo)
 
@@ -36,7 +36,7 @@ def carregar_pilotos():
 
 def salvar_pilotos(pilotos):
 
-        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto CLI/data/pilotos.json", "w", encoding="utf-8") as arquivo:
+        with open("/home/joaopedro/Downloads/Projetos - Python/Aeroporto-CLI/data/pilotos.json", "w", encoding="utf-8") as arquivo:
 
             json.dump(pilotos, arquivo, indent=4, ensure_ascii=False)
 
