f060f09a79e44a0f582a8af782c047f03045a0bc
unknown <nicollausvih@gmail.com>
2026-07-16 09:43:15 -0300

    Inicio_da_implementação_da_API

--- services/torre_service.py
+++ services/torre_service.py
@@ -1,5 +1,19 @@
 from models.mensagem import mensagem
+from API.opensky_service import OpenSkyService
 
 def cadastrar_mensagem(id, remetente, destinatario, conteudo, horario):
     mensagem = mensagem(id, remetente, destinatario, conteudo, horario, [])
     return mensagem
+
+opensky = OpenSkyService()
+
+def monitorar_voos():
+
+    resultado = opensky.buscar_voos()
+
+    if not resultado.success:
+        print(resultado.error)
+        return
+
+    clima = opensky.buscar_voos()
+    print(resultado.data)
